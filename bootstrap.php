0e47fbc0f07a25aac0dda4845dfe7c96cd2022b4
Vinicius <vc896536@gmail.com>
2022-08-19 01:12:39 -0300

    Adjusts

--- bootstrap.php
+++ bootstrap.php
@@ -7,15 +7,16 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-// Development.
-// $config = [
-//   'db_dsn' => 'mysql:host=database;dbname=spelling_bee',
-//   'db_user' => 'root',
-//   'db_pass' => '',
-// ];.
-// Production.
+//Development.
 $config = [
-  'db_dsn' => 'mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_b07eeb49085b99d',
-  'db_user' => 'b0d0821ac5d28d',
-  'db_pass' => '60a475ea',
+  'db_dsn' => 'mysql:host=database;dbname=spelling_bee',
+  'db_user' => 'root',
+  'db_pass' => '',
 ];
+
+// // Production.
+// $config = [
+//   'db_dsn' => 'mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_b07eeb49085b99d',
+//   'db_user' => 'b0d0821ac5d28d',
+//   'db_pass' => '60a475ea',
+// ];
